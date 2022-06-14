9fa085705843a18cc0d1b11a98615f4d681ff638
Victoria LeBel <vickileb@hotmail.com>
2022-06-14 16:47:48 -0700

    commit tree 5739c96b1e3fd23bea11691941afa5562123c565

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Add your code here
+//fetch post
+
+
+// const configurationObjecct = {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json',
+//     },
+//     body: JSON.stringify(formData)
+// };
+function submitData(userName, userEmail){
+   return fetch('http://localhost:3000/users', {
+       method: "POST",
+       headers: {
+           "Content-Type": "application/json",
+           "Accept": "application/json",
+       },
+       body: JSON.stringify({
+            name: userName, 
+            email: userEmail,
+        }),
+   })
+   .then(response => {
+       return response.json()
+   })
+   .then(object => {
+       document.body.innerHTML= object['id']
+   })
+   .catch((error) => {
+      document.body.innerHTML = error.message 
+   })
+}
+
+//console.log(submitData('emily', 'emily@gmail.com'))
+
+
+// fetch('http://localhost:4000/dogs', configurationObjecct)
+// .then(response => {
+//     console.log('hi')
+//     return response.json()
+// })
+// .then(object => console.log(object));
